4c526c22c34a4103d8b4b68715ede82ff2694854
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-02-03 14:41:01 +0100

    .

--- src/core/chart.js
+++ src/core/chart.js
@@ -3,9 +3,16 @@ import {
     Chart as __Chart__,
     CategoryScale,
     LinearScale,
+    LogarithmicScale,
     PointElement,
-    LineElement
+    LineElement,
+
+    Title,
+    SubTitle,
+    Colors
 } from "chart.js";
+
+import { CanvasBackground } from "./plugins/index.js";
 
 class UIChart extends UIElement{
     constructor({ type, data, controllers = [] } = {}){
@@ -18,7 +25,12 @@ class UIChart extends UIElement{
             CategoryScale,
             LinearScale,
             PointElement,
-            LineElement
+            LineElement,
+            LogarithmicScale,
+            Title,
+            SubTitle,
+            Colors,
+            // CanvasBackground
         )
         this.chart = new __Chart__(this.element, {
             responsive: true,
@@ -28,9 +40,73 @@ class UIChart extends UIElement{
         })
         this.style({outline : '2px darkblue dotted'})
     }
+    get data(){
+        return this.chart.data;
+    }
+    setTitle(text, options){
+        // https://www.chartjs.org/docs/latest/configuration/title.html#title-configuration
+        Object.assign(this.chart.options.plugins.title,{
+            text,
+            display : true,
+            ...options
+        })
+        this.chart.update();
+        return this;
+    }
+    setSubTitle(text, options){
+        Object.assign(this.chart.options.plugins.subtitle,{
+            text,
+            display : true,
+            ...options
+        })
+        this.chart.update();
+        return this;
+    }
     register(...controllers){
         this.chart.register(...controllers);
         return this;
+    }
+    stop(){
+        this.chart.stop();
+        return this;
+    }
+    reset(){
+        this.chart.reset();
+        return this;
+    }
+    // render(){
+    //     this.chart.render();
+    //     return this;
+    // }
+    destroy(){
+        this.chart.stop();
+        return this;       
+    }
+    update(mode = null){
+        this.chart.update(mode);
+        return this;        
+    }
+    clear(){
+        this.chart.clear();
+        return this;
+    }
+    resize(w, h){
+        this.size(w, h)
+        // this.chart.resize(w, h);
+        return this;
+    }
+    responsify(res = true){
+        this.chart.options.responsive = res;
+        return this;
+    }
+    size(w, h){
+
+    }
+    toBase64Image(type, quality){
+        return this.chart.toBase64Image()
+    }
+    get img(){
+        return new UIElement({ element : 'img'}).setAttr({src : this.toBase64Image()})
     }
 }
 
---
+++ src/core/plugins/background.js
@@ -0,0 +1,11 @@
+export const CanvasBackground = {
+  id: 'background',
+  beforeDraw: (chart, args, options) => {
+    const {ctx} = chart;
+    ctx.save();
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.fillStyle = options.color || '#99ffff';
+    ctx.fillRect(0, 0, chart.width, chart.height);
+    ctx.restore();
+  }
+};
